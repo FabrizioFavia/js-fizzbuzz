6fe7f46748bec35e4c40acfea14bbc7cf67f8aec
FabrizioFavia <personal.favia@gmail.com>
2023-04-17 15:02:06 +0200

    fix(js): updated loop.

--- js/main.js
+++ js/main.js
@@ -2,11 +2,14 @@ for (let i = 1; i < 101; i++) {
     let element = i;
     
     if (element % 3 == 0 && element % 5 == 0){
-        element = "FizzFazz";
+        element = "FizzBuzz";
 
     } else if(element % 3 == 0 ) {
         element = "Fizz";
-    }
+        
+    } else if(element % 5 == 0 ) {
+    element = "Buzz";
+}
 
     console.log(element);
 
