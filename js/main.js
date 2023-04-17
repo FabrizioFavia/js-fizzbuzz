a602cc600821209c9b85d5c5391aef413cbdd413
FabrizioFavia <personal.favia@gmail.com>
2023-04-17 15:43:09 +0200

    feat(js/css): created loop / color classes in css

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
+const boxContainer = document.getElementById("container");
+
 for (let i = 1; i < 101; i++) {
     let element = i;
-    
-    if (element % 3 == 0 && element % 5 == 0){
+
+    if (element % 3 == 0 && element % 5 == 0) {
         element = "FizzBuzz";
 
-    } else if(element % 3 == 0 ) {
+    } else if (element % 3 == 0) {
         element = "Fizz";
-        
-    } else if(element % 5 == 0 ) {
-    element = "Buzz";
-}
 
-    console.log(element);
-
+    } else if (element % 5 == 0) {
+        element = "Buzz";
+    }
+    boxContainer.innerHTML += `<div class="box">${element}</div>"`;
     
 }
