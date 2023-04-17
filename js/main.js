5f4fa68399c981ec422d77f4302311c1b660227a
FabrizioFavia <personal.favia@gmail.com>
2023-04-17 14:59:54 +0200

    feat(js): added for loop.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 for (let i = 1; i < 101; i++) {
-    const element = i;
-    console.log(element); 
+    let element = i;
+    
+    if (element % 3 == 0 && element % 5 == 0){
+        element = "FizzFazz";
+
+    } else if(element % 3 == 0 ) {
+        element = "Fizz";
+    }
+
+    console.log(element);
+
+    
 }
