b79b91da43909624b1fbfc869bc67e9dd98d236c
FabrizioFavia <personal.favia@gmail.com>
2023-04-18 11:29:33 +0200

    correzione esercizio

--- js/main.js
+++ js/main.js
@@ -2,18 +2,22 @@ const boxContainer = document.getElementById("container");
 
 for (let i = 1; i < 101; i++) {
     let element = i;
+    let classe = " ";
 
     if (element % 3 == 0 && element % 5 == 0) {
         element = "FizzBuzz";
+        classe = "lightPink";
 
     } else if (element % 3 == 0) {
         element = "Fizz";
+        classe = "cadet";
 
     } else if (element % 5 == 0) {
         element = "Buzz";
+        classe = "coral";
     }
-    
-    boxContainer.innerHTML +=`<div class="box">${element}</div>"`;
+
+    boxContainer.innerHTML +=`<div class="box ${classe}" >${element}</div>`;
   
     }
     
